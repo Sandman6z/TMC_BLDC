230a9cde1fd05e5f09a075b2df7187e54ed7632d
Sandman6z <zbn0238@163.com>
2023-11-06 16:10:20 +0800

    good

--- User/adc.c
+++ User/adc.c
@@ -41,7 +41,18 @@ void ADC1_MODE_CONFIG(void)
     ADC_Init(ADC1, &ADC_InitStructure);
 
     // ����ADC1 Ϊ239��11���������ڣ�����Ϊ1
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_71Cycles5);
+    /**
+     * @brief 
+     * ADC_Channel_0    BRAKE_DETECT
+     * ADC_Channel_1    AD_SPEED
+     * ADC_Channel_2    MOS_T_ADC
+     * ADC_Channel_3    I_SUM_ADC
+     * ADC_Channel_4    VM_ADC
+     * ADC_Channel_5    TUR_T_ADC
+     * ADC_Channel_8    
+     * ADC_Channel_17   内部通道
+     */
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_71Cycles5);	
     ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_71Cycles5);
     ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 3, ADC_SampleTime_71Cycles5);
     ADC_RegularChannelConfig(ADC1, ADC_Channel_4, 4, ADC_SampleTime_71Cycles5);
@@ -52,14 +63,6 @@ void ADC1_MODE_CONFIG(void)
 
     // Enable the temperature sensor and vref internal channel
     ADC_TempSensorVrefintCmd(ENABLE);
-    /*	ADC_InjectedSequencerLengthConfig(ADC1, 1); //ע�뷽ʽ
-        ADC_InjectedChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_71Cycles5);	// ADC1 injected channel Configuration
-        ADC_ExternalTrigInjectedConvConfig(ADC1, ADC_ExternalTrigInjecConv_None);		// ADC1 injected external trigger configuration
-        ADC_InjectedDiscModeCmd(ADC1, DISABLE);
-
-        ADC_ExternalTrigInjectedConvCmd(ADC1, ENABLE); 
-    */
-
     ADC_DMACmd(ADC1, ENABLE);                       // Enable ADC1 DMA
     ADC_Cmd(ADC1, ENABLE);                          // Enable ADC1
     ADC_ResetCalibration(ADC1);                     // ��λУ׼�Ĵ���
@@ -72,67 +75,4 @@ void ADC1_MODE_CONFIG(void)
 
     /* Enable DMA channel1 */
     DMA_Cmd(DMA1_Channel1, ENABLE);                 // ��ADC_SoftwareStartConvCmd(ADC1, ENABLE);��Ȼ���׳���ADC�ɼ����ִ�λ
-}
-
-void ADCInit(void)
-{
-    ADC_InitTypeDef ADC_InitStructure;              // ADC????????
-    DMA_InitTypeDef DMA_InitStructure;              // DMA????????
-    /* Enable DMA2 clock */
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
-
-    /* DMA1 channel1 configuration ----------------------------------------------*/
-    DMA_DeInit(DMA1_Channel1);
-    DMA_InitStructure.DMA_PeripheralBaseAddr    = ADC1_BASE + 0x4c;                	// DMA????????
-    DMA_InitStructure.DMA_MemoryBaseAddr        = (uint32_t)&ADCConvertedValue;     //???????
-    DMA_InitStructure.DMA_DIR                   = DMA_DIR_PeripheralSRC;            // DMA??????SRC??,????????
-    DMA_InitStructure.DMA_BufferSize            = 5;                                // DMA????,1?,???DMA_MemoryDataSize
-    DMA_InitStructure.DMA_PeripheralInc         = DMA_PeripheralInc_Disable;        //???????,????????
-    DMA_InitStructure.DMA_MemoryInc             = DMA_MemoryInc_Enable;             //?????????,????????
-    DMA_InitStructure.DMA_PeripheralDataSize    = DMA_PeripheralDataSize_HalfWord;	//?????????16?
-    DMA_InitStructure.DMA_MemoryDataSize        = DMA_MemoryDataSize_HalfWord;      // DMA?????,HalfWord???16?
-    DMA_InitStructure.DMA_Mode                  = DMA_Mode_Circular;                //????,???????
-    DMA_InitStructure.DMA_Priority              = DMA_Priority_High;                // DMA????
-    DMA_InitStructure.DMA_M2M                   = DMA_M2M_Disable;                  // M2M????
-    DMA_Init(DMA1_Channel1, &DMA_InitStructure);
-
-    /* Enable DMA1 channel1 */
-    DMA_Cmd(DMA1_Channel1, ENABLE);
-
-    /* Enable ADC1 and GPIOC clock */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-
-    /* ADC1 configuration ------------------------------------------------------*/
-    ADC_InitStructure.ADC_Mode                  = ADC_Mode_Independent;       //???????
-    ADC_InitStructure.ADC_ScanConvMode          = ENABLE;                     //??????
-    ADC_InitStructure.ADC_ContinuousConvMode    = ENABLE;                  		//????????
-    ADC_InitStructure.ADC_ExternalTrigConv      = ADC_ExternalTrigConv_None;	// ADC????,????
-    ADC_InitStructure.ADC_DataAlign             = ADC_DataAlign_Right;        //????,ADC?12??,?????
-    ADC_InitStructure.ADC_NbrOfChannel          = 5;                          //?????,1?
-    ADC_Init(ADC1, &ADC_InitStructure);
-
-    /* ADC1 regular channel10 configuration ADC???, ?10??? ????1,???? */
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_71Cycles5);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_71Cycles5);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 3, ADC_SampleTime_71Cycles5);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_4, 4, ADC_SampleTime_71Cycles5);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 5, ADC_SampleTime_71Cycles5);
-
-    /* Enable ADC1 DMA */
-    ADC_DMACmd(ADC1, ENABLE); 							// ADC??,??
-    /* Enable ADC1 */
-    ADC_Cmd(ADC1, ENABLE); 									//??ADC1
-
-    /* Enable ADC1 reset calibaration register */
-    ADC_ResetCalibration(ADC1); 						//????
-    /* Check the end of ADC1 reset calibration register */
-    while (ADC_GetResetCalibrationStatus(ADC1))
-        ; 																	//????????
-    /* Start ADC1 calibaration */
-    ADC_StartCalibration(ADC1); 						//????
-    /* Check the end of ADC1 calibration */
-    while (ADC_GetCalibrationStatus(ADC1))
-        ; 																	//??????
-    /* Start ADC1 Software Conversion */
-    ADC_SoftwareStartConvCmd(ADC1, ENABLE); //??????,ADC??DMA???????RAM??
 }
